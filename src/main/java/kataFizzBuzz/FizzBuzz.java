f67dc1c48a6483c005f747e7aa0c3a9e63b18f4b
ahbensal <ahbensal@LFR020838.corp.capgemini.com>
2017-07-21 11:29:57 +0200

    [refacto] refacto du code est limitation des nombres à tester

--- src/main/java/kataFizzBuzz/FizzBuzz.java
+++ src/main/java/kataFizzBuzz/FizzBuzz.java
@@ -4,16 +4,26 @@ public class FizzBuzz {
 
 
 	public String verifierLeNombre(int i) {
-		if(i%3==0 && i%5!=0){
-			return "Fizz";
+		
+		return calculDuRestDeDivision(i);
+	}
+
+	private String calculDuRestDeDivision(int i) {
+		String resultat = null;
+		if(i>0 && i<=100){
+			if(i%3==0 && i%5!=0){
+				resultat = "Fizz";
+			}
+			else if (i%5==0 && i%3!=0){
+				resultat = "Buzz";
+			}
+			else if (i%5==0 && i%3==0){
+				resultat = "FizzBuzz";
+			}
+			else resultat = String.valueOf(i);
 		}
-		else if (i%5==0 && i%3!=0){
-			return "Buzz";
-		}
-		else if (i%5==0 && i%3==0){
-			return "FizzBuzz";
-	    }
-		else return String.valueOf(i);
+		return resultat;
+		
 	}
 
 }
