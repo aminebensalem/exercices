52c8e1a3d5b90893e9554d2a284719b877694f0d
ahbensal <ahbensal@LFR020838.corp.capgemini.com>
2017-07-21 10:55:33 +0200

    [Refacto] suppression du test et de la méthode sans arguments

--- src/main/java/kataFizzBuzz/FizzBuzz.java
+++ src/main/java/kataFizzBuzz/FizzBuzz.java
@@ -2,10 +2,6 @@ package kataFizzBuzz;
 
 public class FizzBuzz {
 
-	public String verifierLeNombre() {
-		
-		return "1";
-	}
 
 	public String verifierLeNombre(int i) {
 		if(i%3==0){
--- src/test/java/kataFizzBuzz/TestFizzBuzz.java
+++ src/test/java/kataFizzBuzz/TestFizzBuzz.java
@@ -4,14 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestFizzBuzz {
-	@Test
-	public void testFizzBuzzdeUn() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-
-		String resultat=fizzBuzz.verifierLeNombre();
-
-		Assert.assertEquals("1", resultat);
-	}
 	
 	@Test
 	public void testFizzDeUn(){
