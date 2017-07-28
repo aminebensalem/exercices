0a2ca100c3c222adca46f4357f7380bb2fad62a1
ahbensal <ahbensal@LFR020838.corp.capgemini.com>
2017-07-28 11:01:42 +0200

    [refacto] réctification de la classe pour l'exploitation du map

--- src/main/java/romanNumerals/RomanNumerals.java
+++ src/main/java/romanNumerals/RomanNumerals.java
@@ -2,16 +2,20 @@ package romanNumerals;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class RomanNumerals {
 
 	public String conversionDuChiffreEnRomain(int i) {
 		Map<Integer,String> conv = new HashMap<Integer,String>();
 		conv.put(1, "I");
+		conv.put(2, "II");
 		String resultat ="";
-		for(Map.Entry m:conv.entrySet()){
-			resultat = (String) m.getValue();
-		}
+		for (Entry<Integer,String> m: conv.entrySet()){
+			if(m.getKey()==i){
+				resultat = (String) m.getValue();
+			}
+		}		
 		return resultat;
 	}
 
