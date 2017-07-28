8595e17baa3129965245257a65e2c3dd990baa32
ahbensal <ahbensal@LFR020838.corp.capgemini.com>
2017-07-28 11:19:15 +0200

    [impl] remplissage du map

--- src/main/java/romanNumerals/RomanNumerals.java
+++ src/main/java/romanNumerals/RomanNumerals.java
@@ -12,6 +12,12 @@ public class RomanNumerals {
 		conv.put(2, "II");
 		conv.put(3, "III");
 		conv.put(4, "IV");
+		conv.put(5, "V");
+		conv.put(6, "VI");
+		conv.put(7, "VII");
+		conv.put(8, "VIII");
+		conv.put(9, "IX");
+		conv.put(10, "X");
 		String resultat ="";
 		for (Entry<Integer,String> m: conv.entrySet()){
 			if(m.getKey()==i){
