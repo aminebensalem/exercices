91fb558e2b9cde290f7e3983b4a08c6b4b0dc2e2
ahbensal <ahbensal@LFR020838.corp.capgemini.com>
2017-08-04 10:35:49 +0200

    [refac] refacto de la méthode convertNumber

--- src/main/java/romanNumeralsII/RomanNumeralsII.java
+++ src/main/java/romanNumeralsII/RomanNumeralsII.java
@@ -3,12 +3,10 @@ package romanNumeralsII;
 public class RomanNumeralsII {
 
 	public String convertNumber(String number) {
-		if(number.equals("1")){
-			return "I";
-		} else if(number.equals("2")){
-			return "II";
+		String resultat="";
+		for(int i = 1 ; i <= Integer.valueOf(number) ; i += 1 ){
+			resultat += "I";
 		}
-		else
-			return "III";
+		return resultat;
 	}	
 }
